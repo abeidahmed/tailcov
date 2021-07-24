3b1d276ed0aba1b4a96c7276ba8352215714042c
abeidahmed <abeidahmed92@gmail.com>
2021-07-24 15:14:06 +0530

    fix errors

--- dummy/config/environments/development.rb
+++ dummy/config/environments/development.rb
@@ -1,12 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-# rubocop:disable Metrics/BlockLength
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
-  # In the development environment your application's code is reloaded any time
-  # it changes. This slows down response time but is perfect for development
-  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Do not eager load code on boot.
@@ -71,8 +65,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  # Uncomment if you wish to allow Action Cable access from any origin.
-  # config.action_cable.disable_request_forgery_protection = true
 end
-# rubocop:enable Metrics/BlockLength
